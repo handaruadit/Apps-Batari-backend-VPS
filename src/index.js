e2c62ed8734619782ee63f72d3861c7ec1cf91d4
handaruadit <adithandaru34@gmail.com>
2026-06-05 15:01:45 +0700

    h.patch Change Port to 3001

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 initSocket(server);
 startAutomaticPlantDataSender();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const HOST = '0.0.0.0';
 
 server.listen(PORT, HOST, () => {
